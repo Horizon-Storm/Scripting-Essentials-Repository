eea8853d6da9c06f0e0e5280d597f8630857d10c
Horizon Storm <jdogmadigan@gmail.com>
2024-09-12 13:57:12 -0600

    Updated the Transform Code
    I updated the Transform Code adding public variables for rotating on all axis, movement distance, and movement speed. It was also changed to move right to left. Previous up and down was commented out.

--- Game Objects and Transform/Assets/Scripts/TransformController.cs
+++ Game Objects and Transform/Assets/Scripts/TransformController.cs
@@ -3,15 +3,32 @@ using UnityEngine;
 
 public class TransformController : MonoBehaviour
 {
+    //Public Rotation Variables
+    public float rotateX = 30;
+    public float rotateY = 30;
+    public float rotateZ = 30;
+    
+    //Public Movement Variables
+    public float distanceX = 5;
+    
+    //Public Speed Variable
+    public float moveSpeed = 1;
     private void Update()
     {
         //Move the target GameObject
-        var x = Mathf.PingPong(Time.time, 3);
-        var p = new Vector3(0, x, 0);
-        transform.position = p;
+        //var x = Mathf.PingPong(Time.time, 3);
+        //var p = new Vector3(0, x, 0);
+        //transform.position = p;
+        
+        //Move Side to side
+        var moveX = Mathf.PingPong(Time.time * moveSpeed, distanceX); 
+        
+        transform.position = new Vector3(moveX, 0, 0);
+
+
 
 
         //Rotate the target Game Object
-        transform.Rotate(new Vector3(0, 30, 0) * Time.deltaTime);
+        transform.Rotate(new Vector3(rotateX, rotateY, rotateZ) * Time.deltaTime);
     }
 }
