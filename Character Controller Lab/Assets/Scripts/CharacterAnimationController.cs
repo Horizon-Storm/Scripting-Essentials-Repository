e3ebadcaf3e8f22cdcc431e5e61538ca18a237cf
Horizon_Storm <jdogmadigan@gmail.com>
2024-10-02 17:59:55 -0600

    Created Animation Controller in Character Controller Project
    Created the ability to hit specific keys and play animations (WDHFSpace) Run will also play running animation.

---
+++ Character Controller Lab/Assets/Scripts/CharacterAnimationController.cs
@@ -0,0 +1,64 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterAnimationController : MonoBehaviour
+{
+    private Animator animator;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Cache the animator component attached to Character
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        HandleAnimations();
+    }
+
+    private void HandleAnimations()
+    {
+        //Handle Running and Idling
+        if (Input.GetAxis("Horizontal") != 0)
+        {
+            animator.SetTrigger("Run");
+        }
+        else
+        {
+            animator.SetTrigger("Idle");
+        }
+        
+        //Handle Jumping
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            animator.SetTrigger("Jump");
+        }
+        
+        //Handle Wall Jump
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            animator.SetTrigger("WallJump");
+        }
+        
+        //Handle Double Jump
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            animator.SetTrigger("DoubleJump");
+        }
+        
+        //Handle Falling
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            animator.SetTrigger("Fall");
+        }
+        
+        //Handle Getting Hit
+        if (Input.GetKeyDown(KeyCode.H))
+        {
+            animator.SetTrigger("Hit");
+        }
+    }
+}
