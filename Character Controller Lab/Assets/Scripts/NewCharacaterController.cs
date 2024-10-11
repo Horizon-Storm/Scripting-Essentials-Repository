4e976a55861d6c50f1671ba619eac5a7241258c1
Horizon Storm <jdogmadigan@gmail.com>
2024-10-11 17:05:07 -0600

    Implemented new Character Controller
    Found a tutorial to create a 2D character controller and followed it to make it so the character would jump and move nicely. FlipController script is no longer needed and can be removed. Character now moves and jumps.

--- Character Controller Lab/Assets/Scripts/NewCharacaterController.cs
+++ Character Controller Lab/Assets/Scripts/NewCharacaterController.cs
@@ -5,27 +5,116 @@ using UnityEngine;
 
 public class NewCharacaterController : MonoBehaviour
 {
-    private float speed = 20f;
+    [SerializeField]
+    private float speed = 3.4f;
+    [SerializeField]
+    private float jumpHeight = 6.5f;
+    public float gravityScale = 1.5f;
     private float horizontalInput;
-    private Rigidbody2D rb;
+    public Camera mainCamera;
+
+    private float moveDirection = 0;
+    bool facingRight = true;
+    bool isGrounded = false;
+    Vector3 cameraPos;
+    Rigidbody2D rb;
+    CapsuleCollider2D mainCollider;
+    Transform t;
     
     // Start is called before the first frame update
     void Start()
     {
+        t = transform;
         //Assign the Rigidbody2D component
         rb = GetComponent<Rigidbody2D>();
-    }
+        //Assign Collider Component
+        mainCollider = GetComponent<CapsuleCollider2D>();
+        //Freeze rotation on the player
+        rb.freezeRotation = true;
+        rb.collisionDetectionMode = CollisionDetectionMode2D.Continuous;
+        rb.gravityScale = gravityScale;
 
-    private void FixedUpdate()
-    {
-        horizontalInput = Input.GetAxis("Horizontal");
-        float horizontalMovement = horizontalInput * speed * Time.deltaTime;
-        rb.velocity = new Vector2(horizontalMovement, rb.velocity.y);
+        if (mainCamera)
+        {
+            cameraPos = mainCamera.transform.position;
+        }
     }
+    
 
     // Update is called once per frame
     void Update()
     {
+        //Movement Controls
+        if ((Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.D)) && (isGrounded || Mathf.Abs(rb.velocity.x) < 0.01f))
+        {
+            moveDirection = Input.GetKey(KeyCode.A) ? -1 : 1;
+        }
+        else
+        {
+            if (isGrounded || rb.velocity.magnitude < 0.0f)
+            {
+                moveDirection = 0;
+            }
+        }
         
+        //Change facing direction
+        if (moveDirection != 0)
+        {
+            if (moveDirection > 0 && !facingRight)
+            {
+                facingRight = true;
+                t.localScale = new Vector3(Mathf.Abs(t.localScale.x), t.localScale.y, t.localScale.z);
+            }
+
+            if (moveDirection < 0 && facingRight)
+            {
+               facingRight = false;
+               t.localScale = new Vector3(-Mathf.Abs(t.localScale.x), t.localScale.y, t.localScale.z);
+            }
+        }
+        
+        //Jumping
+        if (Input.GetKeyDown(KeyCode.W) && isGrounded)
+        {
+            rb.velocity = new Vector2(rb.velocity.x, jumpHeight);
+        }
+        
+        //Camera Follow
+        if (mainCamera)
+        {
+            mainCamera.transform.position = new Vector3(cameraPos.x, cameraPos.y, cameraPos.z);
+        }
+        
+    }
+    
+    private void FixedUpdate()
+    {
+        Bounds colliderBounds = mainCollider.bounds;
+        float colliderRadius = mainCollider.size.x * 0.4f * Mathf.Abs(transform.localScale.x);
+        Vector3 groundCheckPos =
+            colliderBounds.min + new Vector3(colliderBounds.size.x * 0.5f, colliderRadius * 0.9f, 0);
+        //Check if Player is Grounded
+        Collider2D[] colliders = Physics2D.OverlapCircleAll(groundCheckPos, colliderRadius);
+        //Check if any of the overlapping colliders are not player collider, if so , set isGrounded to true
+        isGrounded = false;
+        if (colliders.Length > 0)
+        {
+            for (int i = 0; i < colliders.Length; i++)
+            {
+                if (colliders[i] != mainCollider)
+                {
+                    isGrounded = true;
+                    break;
+                }
+            }
+        }
+        
+        //Apply movement velocity
+        rb.velocity = new Vector2((moveDirection) * speed, rb.velocity.y);
+        
+        //Simple Debug
+        Debug.DrawLine(groundCheckPos, groundCheckPos - new Vector3(0,colliderRadius,0), isGrounded ? Color.green : Color.red);
+        Debug.DrawLine(groundCheckPos, groundCheckPos - new Vector3(colliderRadius,0,0), isGrounded ? Color.green : Color.red);
+
     }
 }
