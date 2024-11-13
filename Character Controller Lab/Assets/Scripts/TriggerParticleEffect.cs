e333fcf2cd0eab83997e3a393313bf353c415200
Horizon_Storm <jdogmadigan@gmail.com>
2024-11-13 15:05:40 -0700

    Added Particle Effects
    Added a particle effect that triggers when the player hits the damaging object. The particle effect still needs to be edited to actually look good but it is currently functional.

---
+++ Character Controller Lab/Assets/Scripts/TriggerParticleEffect.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(ParticleSystem), typeof(Collider2D))]
+public class TriggerParticleEffect : MonoBehaviour
+{
+    private ParticleSystem _particleSystem; //Reference to the Particle System
+
+    public int particleAmmount = 10; //Exposed Variable for Particle Ammount
+    // Start is called before the first frame update
+    void Start()
+    {
+        _particleSystem = GetComponent<ParticleSystem>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.GetComponent<CharacterController>()) ;//Check if player triggers the event
+        {
+            _particleSystem.Emit(particleAmmount); //Emit specified amount of particles
+        }
+    }
+}
