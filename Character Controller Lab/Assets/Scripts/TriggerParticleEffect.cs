de33f881ef15df54f431cbb70fdf62ee5ba5e197
Horizon Storm <jdogmadigan@gmail.com>
2024-11-17 16:56:28 -0700

    Added multiple particles on delay

--- Character Controller Lab/Assets/Scripts/TriggerParticleEffect.cs
+++ Character Controller Lab/Assets/Scripts/TriggerParticleEffect.cs
@@ -7,7 +7,13 @@ public class TriggerParticleEffect : MonoBehaviour
 {
     private ParticleSystem _particleSystem; //Reference to the Particle System
 
-    public int particleAmmount = 10; //Exposed Variable for Particle Ammount
+    //public int particleAmmount = 10; //Exposed Variable for Particle Ammount
+    
+    public int firstparticleAmmount = 10; //Exposed Variable for first Particle Ammount
+    public int secondparticleAmmount = 20; //Exposed Variable for second Particle Ammount
+    public int thirdparticleAmmount = 30; //Exposed Variable for third Particle Ammount
+    public float delayBetweenEmissions = 0.5f; //Delay time between emissions
+    
     // Start is called before the first frame update
     void Start()
     {
@@ -16,11 +22,31 @@ public class TriggerParticleEffect : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D other)
     {
-        if (other.gameObject.GetComponent<CharacterController>()) ;//Check if player triggers the event
+        if (other.gameObject.GetComponent<CharacterController>()) ;//Check if player triggered event
         {
-            _particleSystem.Emit(particleAmmount); //Emit specified amount of particles
+            StartCoroutine(EmitParticlesCoroutine());
         }
+        
+        //if (other.gameObject.GetComponent<CharacterController>()); //Check if player triggers the event
+        //{
+          //  _particleSystem.Emit(particleAmmount); //Emit specified amount of particles
+      //  }
         //if(other.gameObject.tag == "enemy)
         //{}
+    }
+
+    private IEnumerator EmitParticlesCoroutine()
+    {
+        //first emission
+        _particleSystem.Emit(firstparticleAmmount); //Emit based on exposed variable
+        yield return new WaitForSeconds(delayBetweenEmissions); //Wait specified time
+        
+        //second emission
+        _particleSystem.Emit(secondparticleAmmount); //Emit based on exposed variable
+        yield return new WaitForSeconds(delayBetweenEmissions); //Wait specified time
+        
+        //third emission
+        _particleSystem.Emit(thirdparticleAmmount); //Emit based on exposed variable
+        yield return new WaitForSeconds(delayBetweenEmissions); //Wait specified time
     }
 }
