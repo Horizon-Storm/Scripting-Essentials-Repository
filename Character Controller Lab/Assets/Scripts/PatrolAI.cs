dd74956d427a3c5def0817952214b501a12aefa2
Horizon Storm <jdogmadigan@gmail.com>
2024-12-06 17:30:38 -0700

    Patrol Script partially working
    Patrol goes to one point then back. After that it gets stuck.

--- Character Controller Lab/Assets/Scripts/PatrolAI.cs
+++ Character Controller Lab/Assets/Scripts/PatrolAI.cs
@@ -9,6 +9,7 @@ public class PatrolAI : MonoBehaviour
     public Transform[] waypoints;
     private int currentWaypointIndex;
     private NavMeshAgent agent;
+    public float turnDelay = 7f;
     
     // Start is called before the first frame update
     void Start()
@@ -27,10 +28,28 @@ public class PatrolAI : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (agent.remainingDistance < agent.stoppingDistance)
-        {
-            currentWaypointIndex = (currentWaypointIndex + 1) % waypoints.Length;;
-            agent.SetDestination(waypoints[currentWaypointIndex].position);
-        }
+            //if (agent.remainingDistance <= agent.stoppingDistance)
+           // {
+                //currentWaypointIndex = (currentWaypointIndex + 1) % waypoints.Length;
+                //agent.SetDestination(waypoints[currentWaypointIndex].position);
+           // }
+           // else
+           // {
+                StartCoroutine(turnAround());
+          //  }
+    }
+    private IEnumerator turnAround()
+    {
+        yield return new WaitForSeconds(turnDelay); //Wait specified time
+
+        currentWaypointIndex = 1;
+        agent.SetDestination(waypoints[currentWaypointIndex].position);
+
+        yield return new WaitForSeconds(turnDelay); //Wait specified time
+        
+        currentWaypointIndex = 0;
+        agent.SetDestination(waypoints[currentWaypointIndex].position);
+        
+        yield return new WaitForSeconds(turnDelay);
     }
 }
